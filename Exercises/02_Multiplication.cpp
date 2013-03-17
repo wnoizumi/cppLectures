fec78865ba26a50c5d491710398fd1cdc53fa362
Willian Oizumi <oizumi.willian@gmail.com>
2013-03-17 10:10:21 -0300

    Multiplication Exercise

---
+++ Exercises/02_Multiplication.cpp
@@ -0,0 +1,10 @@
+/* In this exercise you'll write a program that reads two numbers
+ * and print the multiplication of such numbers.
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  //your code here!
+}
