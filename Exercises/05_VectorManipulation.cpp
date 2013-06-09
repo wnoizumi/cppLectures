6ada71f425bfc96f400d61fe903c8a4d155e650d
Willian Oizumi <oizumi.willian@gmail.com>
2013-06-09 12:17:05 -0300

    First Vector exercise

---
+++ Exercises/05_VectorManipulation.cpp
@@ -0,0 +1,15 @@
+/*  Write a program to fill an array with numbers 1-100.
+ *  Then print the numbers on the screen.
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+      
+int main() {
+  vector<int> v; 
+  //your code here!
+  return 0;
+}
+
